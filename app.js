f8a7370347b264df5c69f24a4f9b5e28fe82a0df
Robel Isaias <robelisaias@gmail.com>
2026-04-29 19:15:39 -0400

    added the Api methods

--- app.js
+++ app.js
@@ -207,3 +207,72 @@
 // change STATE → then render UI
 //
 // ======================================================
+
+
+
+async function getPosts() {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = await res.json();
+        return posts;
+
+    } catch (err) {
+        console.error('Error fetching posts:', err);
+        return [];
+    }
+}
+
+
+
+async function createPost(title, body) {
+    const newPost = { title, body };
+
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newPost)
+        });
+
+        const post = await res.json();
+        post.id = Date.now(); // Fake ID just so I override the ID that API gives me
+        return post;
+
+    } catch (err) {
+        console.error('Error creating post:', err);
+        return null;
+    }
+}
+
+
+async function updatePost(id, title, body) {
+    try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title, body })
+        });
+        const updatedPost = await res.json();
+        return updatedPost;
+    } catch (err) {
+        console.error('Error updating post:', err);
+    }
+
+
+}
+
+
+async function deletePost(id) {
+
+    try {
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'DELETE'
+        });
+    } catch (err) {
+        console.error('Error deleting post:', err);
+    }
+}
